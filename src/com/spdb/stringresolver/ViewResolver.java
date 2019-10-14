41f7d9315aa6971c230197e5b68df4bb34750157
leesen934 <31769090+leesen934@users.noreply.github.com>
2019-10-14 15:39:21 +0800

    viewResolver

--- src/com/spdb/stringresolver/ViewResolver.java
+++ src/com/spdb/stringresolver/ViewResolver.java
@@ -1,16 +1,44 @@
 package com.spdb.stringresolver;
 
 public class ViewResolver {
-	public static String NOLOGIN = "请先登录";
-	public static String LOGINSUCCESS = "登录成功，欢迎您 %s";
+	public static String NoLogin = "请先登录";
+	public static String LoginSuccess = "登录成功，欢迎您 %s";
 	public static String NoRegister = "抱歉 %s 未注册";
-	public static String ERRORREGISTER = "该用户已注册";
+	public static String ErrorRegister = "该用户已注册";
 	public static String RegisterSuccess = "注册成功，欢迎您的加入";
-	public static String DepositSuccess = "您已成功充值%s 元";
-	public static String PaySuccess = "您已成功付款%s 元";
-	public static String ERRORPASSWORD = "密码错误";
+	public static String DepositSuccess = "您已成功充值200元";
+	public static String PaySuccess = "您已成功付款200元";
+	public static String ErrorPassword = "密码错误";
 
-	public static String 
+	public static void solveView(String msg) {
+		switch (msg){
+			case "noLogin":
+				System.out.println(NoLogin);
+				break;
+			case "loginSuccess":
+				System.out.println(LoginSuccess);
+				break;
+			case "noRegister":
+				System.out.println(NoRegister);
+				break;
+			case "errorRegister":
+				System.out.println(ErrorRegister);
+				break;
+			case "registerSuccess":
+				System.out.println(RegisterSuccess);
+				break;
+			case "depositSuccess":
+				System.out.println(DepositSuccess);
+				break;
+			case "paySuccess":
+				System.out.println(PaySuccess);
+				break;
+			case "errorPassword":
+				System.out.println(ErrorPassword);
+				break;
+
+		}
+	}
 
 
 }
